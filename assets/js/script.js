3945443bebcac9b4d073fe54ab8a50775fe27d8e
Matt Manning <mtmanning93@hotmail.co.uk>
2023-02-14 11:13:53 +0000

    Add computerHand function enabling computer to randomly choose in the array

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+// enable computer decision
+function computerHand() {
+    const randomNum = Math.floor(Math.random() * choices.length);
+    return choices[randomNum];
+}
+
+// make buttons listen for a click by user
+function userHand() {
+    
+}
+
+// calculate the winner
+function onClick() {
+
+}
+
+// get the result of (computerHand, userHand)
+function getScore() {
+
+}
+
+// show result of computerHand() and userHand() in scoreboard
+function showScore () {
+
+}
+
