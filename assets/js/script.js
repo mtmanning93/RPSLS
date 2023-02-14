fbf4f0e90addb8a3d5ca29144aba7c4f9b40a48e
Matt Manning <mtmanning93@hotmail.co.uk>
2023-02-14 14:09:43 +0000

    Add user hand function to alter user image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+let userImage = document.getElementById('human-hand');
+let compImage = document.getElementById('comp-hand');
+const hands = document.getElementsByClassName('hand-buttons');
 
 // enable computer decision
 function computerHand() {
@@ -6,9 +9,20 @@ function computerHand() {
     return choices[randomNum];
 }
 
+
+// need event listener for the buttons
 // make buttons listen for a click by user
-function userHand() {
-    
+for (let hand of hands) {
+    hand.addEventListener('click', function () {
+        let userChoice = this.getAttribute("value");
+        userHand(userChoice);
+        console.log(userChoice);
+    });
+};
+
+// change user image to hand choice image
+function userHand(userChoice) {
+    userImage.style.backgroundImage = `url(/assets/images/${userChoice}.png)`;
 }
 
 // calculate the winner
